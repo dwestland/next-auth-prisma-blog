13d5d46f4bbc1bb79bb814e2f05b7203b8aa2b17
dwestland <don@westland.net>
2021-10-20 07:26:20 -0700

    Add TypeScript to BlogItem

--- components/BlogItem.tsx
+++ components/BlogItem.tsx
@@ -1,40 +1,29 @@
 import React, { FC } from 'react'
 
-type BlogProps = {
-  article: object
-}
-
-interface Articles {
-  body: string
-  title: string
-  author: {
-    name: string
-    email: string
-  }
-  _count: {
-    blogLike: number
+interface BlogProps {
+  article: {
+    body: string
+    title: string
+    author: {
+      name: string
+      email: string
+    }
+    _count: {
+      blogLike: number
+    }
   }
 }
 
-const BlogItem: FC<BlogProps> = ({ article }): JSX.Element => {
-  console.log('%c article ', 'background: red; color: white', article)
-  console.log(
-    '%c article.title ',
-    'background: red; color: white',
-    article.title
-  )
-  console.log(
-    '%c article.author.name ',
-    'background: red; color: white',
-    article.author.name
-  )
-  const bestName = article.author.name ?? article.author.email
+const BlogItem: FC<BlogProps> = (props: BlogProps): JSX.Element => {
+  const { title, author, _count } = props.article
+
+  const bestName = author.name ?? author.email
 
   return (
     <div>
-      <h3>{article.title}</h3>
+      <h3>{title}</h3>
       <p>By {bestName}</p>
-      <p>Likes {article._count.blogLike}</p>
+      <p>Likes {_count.blogLike}</p>
     </div>
   )
 }
