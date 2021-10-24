aede17485087e16c10fb76150c1217891edc4fc7
dwestland <don@westland.net>
2021-10-24 10:04:06 -0700

    Add React Query Dev Tools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from 'react-query'
 import { AppProps } from 'next/app'
 import React from 'react'
 import '../styles/globals.css'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 const queryClient = new QueryClient()
 
@@ -11,6 +12,7 @@ const App = ({ Component, pageProps }: AppProps) => (
   <Provider session={pageProps.session}>
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </Provider>
 )
