7cab595f1136160e74df64cf6035d906a455c2da
dwestland <don@westland.net>
2021-10-26 21:54:11 -0700

    Update add blog API

--- pages/api/blog/add.ts
+++ pages/api/blog/add.ts
@@ -1,23 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { PrismaClient } from '.prisma/client'
+import { PrismaClient, Prisma } from '.prisma/client'
 
 const prisma = new PrismaClient()
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  // res.status(200).json({ eagle: 'Has landed' })
-  const { title, body, authorId } = req.body
-  console.log('%c title ', 'background: green; color: white', title)
-  console.log('%c body ', 'background: green; color: white', body)
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' })
+  }
 
   try {
-    await prisma.blogs.create({
-      title,
-      body,
-      authorId,
-    })
-    res.status(200).json({ message: 'Blog Saved' })
-  } catch (error) {
-    console.log(error)
-    res.status(403).json({ error: 'Error occurred.' })
+    console.log('%c req.body ', 'background: red; color: white', req.body)
+    const blog: Prisma.AccountCreateInput = JSON.parse(req.body)
+    const savedBlog = await prisma.blog.create({ blog })
+    res.status(200).json(savedBlog)
+  } catch (err) {
+    res.status(400).json({ message: 'Something went wrong' })
   }
 }
