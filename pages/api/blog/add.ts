edd7c35638cbd106661d91fd3130227fdb6c5956
dwestland <don@westland.net>
2021-10-27 22:40:18 -0700

    Fix add blog API

--- pages/api/blog/add.ts
+++ pages/api/blog/add.ts
@@ -8,11 +8,23 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     return res.status(405).json({ message: 'Method not allowed' })
   }
 
+  const { title, body, authorId } = req.body.data
+
+  console.log('%c req.body ', 'background: red; color: white', req.body)
+  console.log('%c title ', 'background: red; color: white', title)
+  console.log('%c body ', 'background: red; color: white', body)
+  console.log('%c authorId ', 'background: red; color: white', authorId)
+
   try {
-    console.log('%c req.body ', 'background: red; color: white', req.body)
-    const blog: Prisma.AccountCreateInput = JSON.parse(req.body)
-    const savedBlog = await prisma.blog.create({ blog })
-    res.status(200).json(savedBlog)
+    await prisma.blogs.create({
+      data: {
+        title,
+        body,
+        authorId,
+      },
+    })
+
+    res.status(200).json({ message: 'eagle has landed' })
   } catch (err) {
     res.status(400).json({ message: 'Something went wrong' })
   }
