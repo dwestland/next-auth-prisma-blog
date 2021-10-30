0a3bd83e9d770e856ed530526cfc7c7d837abfdc
dwestland <don@westland.net>
2021-10-29 21:41:47 -0700

    Add titleCount API

--- pages/api/blog/titleCount.ts
+++ pages/api/blog/titleCount.ts
@@ -4,21 +4,20 @@ import { PrismaClient } from '.prisma/client'
 const prisma = new PrismaClient()
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { title } = req.body.data
+
   if (req.method !== 'GET') {
     return res.status(405).json({ message: 'Method not allowed' })
   }
 
   try {
-    const titleExists: number = await prisma.blogs.count({
+    const titleCount: number = await prisma.blogs.count({
       where: {
-        title: 'xxxx',
+        title,
       },
     })
-    let data = false
-    if (titleExists !== 0) {
-      data = true
-    }
-    // console.log('%c titleExists ', 'background: red; color: white' titleExists)
+
+    const data: object = { data: { count: titleCount } }
     res.status(200).json(data)
   } catch (err) {
     console.log(err)
