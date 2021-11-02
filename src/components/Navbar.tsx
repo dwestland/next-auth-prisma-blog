3444b94e8617f93cb1d671bfc82e81d57be1a897
dwestland <don@westland.net>
2021-11-02 07:52:52 -0700

    Add navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Link from 'next/link'
+import styles from '../../styles/Navbar.module.css'
+
+export default function Navbar() {
+  return (
+    <div>
+      <ul className={styles.ul}>
+        <li className={styles.li}>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li className={styles.li}>
+          <Link href="/blogs">
+            <a>Blogs</a>
+          </Link>
+        </li>
+        <li className={styles.li}>
+          <Link href="/blogs/add">
+            <a>Add Blog</a>
+          </Link>
+        </li>
+      </ul>
+      <hr className={styles.hr} />
+    </div>
+  )
+}
