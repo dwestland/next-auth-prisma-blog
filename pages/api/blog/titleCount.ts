64933ff3ba7d4814c0a9bb7afe9f8cf8b725647c
dwestland <don@westland.net>
2021-11-04 17:32:25 -0700

    Change titleCount method, update UI

--- pages/api/blog/titleCount.ts
+++ pages/api/blog/titleCount.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '.prisma/client'
 const prisma = new PrismaClient()
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== 'GET') {
+  if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' })
   }
 
