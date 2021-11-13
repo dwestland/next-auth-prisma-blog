0868da8c2e3e422ce330d494ede74c4a9e3cccb7
dwestland <don@westland.net>
2021-11-13 10:18:29 -0800

    Add post method to blogs

--- pages/api/blogs.ts
+++ pages/api/blogs.ts
@@ -4,37 +4,82 @@ import { PrismaClient } from '.prisma/client'
 const prisma = new PrismaClient()
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== 'GET') {
-    return res.status(405).json({ message: 'Method not allowed' })
+  // if (req.method !== 'GET') {
+  //   return res.status(405).json({ message: 'Method not allowed' })
+  // }
+
+  if (req.method === 'GET') {
+    try {
+      const articles = await prisma.blogs.findMany({
+        orderBy: {
+          updatedAt: 'desc',
+        },
+        select: {
+          id: true,
+          body: true,
+          title: true,
+          author: {
+            select: {
+              id: true,
+              name: true,
+              email: true,
+            },
+          },
+          _count: {
+            select: {
+              blogLike: true,
+            },
+          },
+        },
+      })
+      res.status(200).json({ articles })
+    } catch (err) {
+      console.log(err)
+      res.status(403).json({ err: 'Error occurred.' })
+    }
+    return null
   }
 
-  try {
-    const articles = await prisma.blogs.findMany({
-      orderBy: {
-        updatedAt: 'desc',
-      },
-      select: {
-        id: true,
-        body: true,
-        title: true,
-        author: {
-          select: {
-            id: true,
-            name: true,
-            email: true,
-          },
+  if (req.method === 'POST') {
+    console.log(
+      '%c req.body.data.user ',
+      'background: red; color: white',
+      req.body.data.user
+    )
+
+    try {
+      const articles = await prisma.blogs.findMany({
+        orderBy: {
+          updatedAt: 'desc',
         },
-        _count: {
-          select: {
-            blogLike: true,
+        select: {
+          id: true,
+          body: true,
+          title: true,
+          author: {
+            select: {
+              id: true,
+              name: true,
+              email: true,
+            },
           },
+          _count: {
+            select: {
+              blogLike: true,
+            },
+          },
+          // include: {
+          //   blogLike: {}
+          //   }
         },
-      },
-    })
-    res.status(200).json({ articles })
-  } catch (err) {
-    console.log(err)
-    res.status(403).json({ err: 'Error occurred.' })
+      })
+      res.status(200).json({ articles })
+    } catch (err) {
+      console.log(err)
+      res.status(403).json({ err: 'Error occurred.' })
+    }
+    return null
   }
-  return null
+
+  return res.status(405).json({ message: 'Method not allowed' })
 }
