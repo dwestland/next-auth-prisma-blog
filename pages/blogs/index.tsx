e44681e7f1b079317481bfcf70d0c4ba235daf0f
dwestland <don@westland.net>
2022-07-07 22:18:03 -0700

    Remove functionaliry

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -1,11 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useQuery } from 'react-query'
 import { useSession } from 'next-auth/client'
 import Link from 'next/link'
-import toast, { Toaster } from 'react-hot-toast'
 import BlogItem from '@/components/BlogItem'
 import Navbar from '@/components/Navbar'
-import styles from '@/styles/Blogs.module.css'
 
 interface Articles {
   articles: {}[]
@@ -29,7 +27,6 @@ interface Article {
 
 export default function Blogs() {
   const [session] = useSession()
-  const [blogType, setBlogType] = useState('All Blogs')
   const url = `${process.env.NEXT_PUBLIC_API}/blogs`
 
   const fetchAllBlogs = async () => {
@@ -65,64 +62,22 @@ export default function Blogs() {
     return <span>Error: {error?.message}</span>
   }
 
-  const handleSelectChange = (e) => {
-    setBlogType(e.target.value)
-  }
-
-  const userLikingOwnError = () => {
-    toast.error('You cannot like your own blog!')
-  }
-
   const result = () => {
     const allBlogs = data.articles.map((article: Article) => (
-      <BlogItem
-        key={article.title}
-        article={article}
-        userLikingOwnError={userLikingOwnError}
-      />
+      <BlogItem key={article.title} article={article} />
     ))
 
-    switch (blogType) {
-      case 'All Blogs':
-        return allBlogs
-      case 'My Blogs':
-        console.log('%c data ', 'background: red; color: white', data)
-        console.log('%c allBlogs ', 'background: red; color: white', allBlogs)
-        return allBlogs.filter(
-          (blog) => blog.props.article.author.id === session.id
-        )
-      // TODO Add like info to Blogs API
-      case 'My Likes':
-        return allBlogs
-      default:
-        return allBlogs
-    }
+    return allBlogs
   }
 
   if (session) {
     return (
       <div className="container">
-        <Toaster
-          toastOptions={{
-            style: {
-              height: '60px',
-              border: '1px solid lightgray',
-            },
-          }}
-        />
         <Navbar />
         <h1>Blogs</h1>
         <Link href="/blogs/add">
           <a className="btn">Add Blog</a>
         </Link>
-        <select
-          onChange={(e) => handleSelectChange(e)}
-          className={styles.blogSelect}
-        >
-          <option value="All Blogs">All Blogs</option>
-          <option value="My Blogs">My Blogs</option>
-          <option value="My Likes">My Likes</option>
-        </select>
         <div>
           {data.articles.length === 0 && <h3>No Articles</h3>}
           {result()}
