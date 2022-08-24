18a30b2155937cc9672894f1791cb994e73efcc9
dwestland <don@westland.net>
2022-08-23 19:40:57 -0700

    Remove unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,31 @@
 import React from 'react'
-import { useSession } from 'next-auth/react'
 import Image from 'next/image'
 import Layout from '@/components/Layout'
 
-const HomePage = () => {
-  const { data: session } = useSession()
-
-  // const { data: session, status } = useSession()
-  // const loading = status === 'loading'
-
-  if (session) {
-  }
-  return (
-    <Layout title="Document" description="Document description">
-      <main>
-        <section>
-          <h1>Welcome to App</h1>
-          <h3>Section</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
-            obcaecati quam commodi perspiciatis aut sequi, corporis numquam
-            reiciendis necessitatibus eligendi deserunt voluptatem iste ea
-            delectus magnam. Excepturi, esse suscipit facere dolore deleniti
-            temporibus odit inventore, beatae possimus quae placeat quasi
-          </p>
-          <Image src="/images/ibm.jpg" width={600} height={400} alt="ENIAC" />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
-            obcaecati quam commodi perspiciatis aut sequi, corporis numquam
-            reiciendis necessitatibus eligendi deserunt voluptatem iste ea
-            delectus magnam. Excepturi, esse suscipit facere dolore deleniti
-            temporibus odit inventore, beatae possimus quae placeat quasi
-          </p>
-        </section>
-      </main>
-    </Layout>
-  )
-}
+const HomePage = () => (
+  <Layout title="Document" description="Document description">
+    <main>
+      <section>
+        <h1>Welcome to App</h1>
+        <h3>Section</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+          obcaecati quam commodi perspiciatis aut sequi, corporis numquam
+          reiciendis necessitatibus eligendi deserunt voluptatem iste ea
+          delectus magnam. Excepturi, esse suscipit facere dolore deleniti
+          temporibus odit inventore, beatae possimus quae placeat quasi
+        </p>
+        <Image src="/images/ibm.jpg" width={600} height={400} alt="ENIAC" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+          obcaecati quam commodi perspiciatis aut sequi, corporis numquam
+          reiciendis necessitatibus eligendi deserunt voluptatem iste ea
+          delectus magnam. Excepturi, esse suscipit facere dolore deleniti
+          temporibus odit inventore, beatae possimus quae placeat quasi
+        </p>
+      </section>
+    </main>
+  </Layout>
+)
 
 export default HomePage
